3c85fc6782641f04076ad3f2422d7acfe8c24aa5
vyrus <vertigo1starshooter@gmail.com>
2016-06-14 10:22:23 -0700

    menu code cleanup (mostly removing old shit)

--- content/dota_addons/trap_wars/panorama/scripts/custom_game/tooltips/menu_list_item_tooltip.js
+++ content/dota_addons/trap_wars/panorama/scripts/custom_game/tooltips/menu_list_item_tooltip.js
@@ -3,24 +3,6 @@
     var panel = $.GetContextPanel();
 
     // set the info
-    //var child;
-
-    //child = panel.FindChildTraverse("header_image");
-    //child.style["background-image"] = "url('"+panel._args["image"]+"');";
-
-    //child = panel.FindChildTraverse("title_text");
-    //child.text = $.Localize(panel._args["title"]);
-
-    //child = panel.FindChildTraverse("gold_text");
-    //child.text = $.Localize(panel._args["gold"]);
-
-    //child = panel.FindChildTraverse("class_text");
-    //child.text = $.Localize(panel._args["class"]);
-    //child.style.color = $.Localize(panel._args["class"]+"_color");
-
-    //child = panel.FindChildTraverse("menu_description");
-    //child.text = $.Localize(panel._args["description"]);
-
     Config.SetChildTextTraverse(panel, "title_text"      , $.Localize(panel._args.title      ));
     Config.SetChildTextTraverse(panel, "gold_text"       , $.Localize(panel._args.gold       ));
     Config.SetChildTextTraverse(panel, "class_text"      , $.Localize(panel._args.class      ));
